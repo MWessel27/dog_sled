b3de2c885023db0afda76feab0ff7bc554d48499
Mikalangelo Wessel <mwessel27@gmail.com>
2020-01-05 10:31:22 -0500

    Moving object around screen with gesture recognizer

--- dogsled/dogsled/MainViewController.swift
+++ dogsled/dogsled/MainViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class MainViewController: UIViewController {
     
     @IBOutlet weak var topLeftCircleImageView: UIImageView!
+    @IBOutlet weak var bottomRightCircleImageView: UIImageView!
+    
+    var fileViewOrigin: CGPoint!
     
 
     override func viewDidLoad() {
@@ -19,8 +22,37 @@ class MainViewController: UIViewController {
         
         topLeftCircleImageView.layer.masksToBounds = true
         topLeftCircleImageView.layer.cornerRadius = topLeftCircleImageView.bounds.width / 2
+        
+        bottomRightCircleImageView.layer.masksToBounds = true
+        bottomRightCircleImageView.layer.cornerRadius = bottomRightCircleImageView.bounds.width / 2
+        
+        addPanGesture(view: topLeftCircleImageView)
+        fileViewOrigin = topLeftCircleImageView.frame.origin
     }
 
+    func addPanGesture(view: UIView) {
+        print("huh")
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(MainViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+        
+    }
+    
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let fileView = sender.view!
+        let translation = sender.translation(in: view)
+        
+        switch sender.state {
+        case .began, .changed:
+            fileView.center = CGPoint(x: fileView.center.x + translation.x, y: fileView.center.y + translation.y)
+            sender.setTranslation(CGPoint.zero, in: view)
+        case .ended:
+            break
+        default:
+            break
+        }
+        
+        
+    }
 
 }
 
